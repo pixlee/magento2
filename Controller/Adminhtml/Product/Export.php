6c82a419ba51798803fd68b298550e02ea39d850
Satnam Dhanoa <satnam@Pixlees-MacBook-Pro-3.local>
2016-01-27 11:06:13 -0800

    add a method to save products locally

--- Controller/Adminhtml/Product/Export.php
+++ Controller/Adminhtml/Product/Export.php
@@ -26,6 +26,31 @@ class Export extends \Magento\Backend\App\Action
       $this->_logger            = $logger;
   }
 
+  public function saveProductLocally($product, $response)
+  {
+    $albumId = 0;
+    if(isset($response->data->album->id)) {
+      $albumId = $response->data->album->id;
+    } else if(isset($response->data->product->album_id)) {
+      $albumId = $response->data->product->album_id;
+    }
+
+    if($albumId) {
+      try {
+        $album = $this->_objectManager->create('Pixlee\Pixlee\Model\Album');
+        $album->setProductId($product->getId());
+        $album->setPixleeAlbumId($albumId);
+        $album->save();
+        $this->_logger->addInfo("Product saved successfully. ID is {$product->getId()}");
+        return true;
+      } catch (Exception $e) {
+        $this->_logger->addInfo($e->getMessage());
+      }
+    } else {
+      $this->_logger->addInfo("Product not saved");
+      return false;
+    }
+  }
 
   public function execute()
   {
@@ -37,7 +62,8 @@ class Export extends \Magento\Backend\App\Action
         if(isset($ids[0])) {
           $product->getStoreId($ids[0]);
         }
-        $this->_pixleeData->exportProductToPixlee($product);
+        $response = $this->_pixleeData->exportProductToPixlee($product);
+        $this->saveProductLocally($product, $response);
       }
 
       $resultJson = $this->resultJsonFactory->create();
