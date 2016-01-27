9350a50ab1afaac90ab246c5b82fb68895152c02
Satnam Dhanoa <satnam@Pixlees-MacBook-Pro-3.local>
2016-01-27 11:12:45 -0800

    return response after exporting to pixlee

--- Helper/Data.php
+++ Helper/Data.php
@@ -10,6 +10,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
   protected $_catalogProduct;
   protected $_mediaConfig;
   protected $_scopeConfig;
+  protected $_objectManager;
   protected $_logger;
 
   const ANALYTICS_BASE_URL = 'https://limitless-beyond-4328.herokuapp.com/events/';
@@ -30,6 +31,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
       \Magento\Catalog\Model\Product\Media\Config $mediaConfig,
       \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
       \Psr\Log\LoggerInterface $logger,
+      \Magento\Framework\ObjectManager\ObjectManager $objectManager,
+      \Magento\Framework\View\Result\PageFactory $pageFactory,
       \Magento\Framework\Pricing\Helper\Data $pricingHelper,
       \Magento\Directory\Model\Region $directoryRegion
       ){
@@ -41,7 +44,9 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
       $this->_catalogProduct    = $catalogProduct;
       $this->_mediaConfig       = $mediaConfig;
       $this->_scopeConfig       = $scopeConfig;
+      $this->_objectManager     = $objectManager;
       $this->_logger            = $logger;
+      $this->_pageFactory       = $pageFactory;
       $this->_pricingHelper     = $pricingHelper;
       $this->_directoryRegion   = $directoryRegion;
 
@@ -135,128 +140,130 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
       if($this->isInactive() || !isset($productName)) {
         return false;
       }
+
       $pixlee = $this->_pixleeAPI;
-        if($product->getVisibility() != 1) { // Make sure the product is visible in search or catalog
-          $product_mediaurl = $this->_mediaConfig->getMediaUrl($product->getImage());
-          $response = $pixlee->createProduct($product->getName(), $product->getSku(), $product->getProductUrl(), $product_mediaurl);
-        }
-
-        return true;
-      }
-
-      public function _sendPayload($event, $payload) {
-        if($payload && isset($this->_urls[$event])) {
-          $ch = curl_init($this->_urls[$event]);
-
-          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-          curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/x-www-form-urlencoded'));
-
-          $response   = curl_exec($ch);
-          $responseInfo   = curl_getinfo($ch);
-          $responseCode   = $responseInfo['http_code'];
-          curl_close($ch);
-
-          if( !$this->isBetween($responseCode, 200, 299) ) {     
-            $this->_logger->addInfo("HTTP $responseCode response from Pixlee API");
-          } elseif ( is_object($response) && is_null( $response->status ) ) {
-            $this->_logger->addInfo("Pixlee did not return a status");
-          } elseif( is_object($response) && !$this->isBetween( $response->status, 200, 299 ) ) {
-            $errorMessage   = implode(',', (array)$response->message);
-            $this->_logger->addInfo("$response->status - $errorMessage ");
-          } else {
-            $this->_logger->addInfo("Analytics event sent");
-            return true;
-          }
-        }
-        $this->_logger->addInfo("Analytics event not sent ".json_encode($payload));
-        return false;
-      }
-
-      public function _extractProduct($product) {
-        $productData = array();
-        $productData['quantity'] = $product->getQty();
-
-        if($product->getId()) {
-          $productData['id']    = $product->getId();
-          $productData['name']  = $product->getName();
-          $productData['sku']   = $product->getSku();
-          $productData['price'] = $this->_pricingHelper->currency($product->getPrice(), true, false); // Get price in the main currency of the store. (USD, EUR, etc.)
-        }
-
-        return $productData;
-      }
-
-      public function _extractCart($quote) {
-        if(is_a($quote, '\Magento\Sales\Model\Quote') || is_a($quote, '\Magento\Sales\Model\Order')) {
-          $cartData = array('products' => array());
-          foreach ($quote->getAllItems() as $item) {
-            $cartData['products'][] = $this->_extractProduct($item);
-          }
-          $cartData['total'] = $quote->getGrandTotal();
-          return $cartData;
-        }
-        return false;
-      }
-
-      public function _extractCustomer($quote) {
-        if(is_a($quote, '\Magento\Sales\Model\Order')) {
-          return array(
-            'name'      => $quote->getShippingAddress()->getName(),
-            'email'     => $quote->getShippingAddress()->getEmail(),
-            'city'      => $quote->getShippingAddress()->getCity(),
-            'state'     => $this->_directoryRegion->load($quote->getShippingAddress()->getRegionId())->getName(),
-            'country'   => $quote->getShippingAddress()->getCountry()
-            );
-        }
-        return false;
-      }
-
-      public function _validateCredentials() {
-        try{
-          $this->_pixleeAPI->getAlbums();
-        }catch(Exception $e){
-          throw new \Magento\Framework\Exception\LocalizedException(__('You may have entered the wrong credentials. Please check again.'));
-        }
-      }
-
-      public function _preparePayload($extraData = array()) {
-        if(($payload = $this->_getPixleeCookie()) && $this->isActive()) {
-            // Append all extra data to the payload
-          foreach($extraData as $key => $value) {
-                // Don't accidentally overwrite existing data.
-            if(!isset($payload[$key])) {
-              $payload[$key] = $value;
-            }
-          }
-            // Required key/value pairs not in the payload by default.
-          $payload['API_KEY']= $this->getApiKey();
-          $payload['uid'] = $payload['CURRENT_PIXLEE_USER_ID'];
-          $payload['pixlee_album_photos_timestamps'] = $payload['CURRENT_PIXLEE_ALBUM_PHOTOS_TIMESTAMP'];
-          $payload['pixlee_album_photos'] = $payload['CURRENT_PIXLEE_ALBUM_PHOTOS'];
-          $payload['horizontal_page'] = $payload['HORIZONTAL_PAGE'];
-          return http_build_query($payload);
-        }
-        return false; // No cookie exists, 
-      }
-
-      protected function _getPixleeCookie() {
-        if(isset($_COOKIE['pixlee_analytics_cookie'])){
-          if($cookie = $_COOKIE['pixlee_analytics_cookie']) {
-                // Return the decoded cookie as an associative array, not a PHP object
-                // as json_decode prefers.
-            return json_decode($cookie, true);
-          }
-        }
-        return false;
-      }
-
-      protected function isBetween($theNum, $low, $high) {
-        if($theNum >= $low && $theNum <= $high) {
-          return true;
-        } else {
-          return false;
-        }
+      if($product->getVisibility() != 1) { // Make sure the product is visible in search or catalog
+        $product_mediaurl = $this->_mediaConfig->getMediaUrl($product->getImage());
+        $response = $pixlee->createProduct($product->getName(), $product->getSku(), $product->getProductUrl(), $product_mediaurl);
+        $this->_logger->addInfo("Product Exported to Pixlee");
+        return $response;
       }
     }
+
+    public function _sendPayload($event, $payload) {
+      if($payload && isset($this->_urls[$event])) {
+        $ch = curl_init($this->_urls[$event]);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/x-www-form-urlencoded'));
+
+        $response   = curl_exec($ch);
+        $responseInfo   = curl_getinfo($ch);
+        $responseCode   = $responseInfo['http_code'];
+        curl_close($ch);
+
+        if( !$this->isBetween($responseCode, 200, 299) ) {
+          $this->_logger->addInfo("HTTP $responseCode response from Pixlee API");
+        } elseif ( is_object($response) && is_null( $response->status ) ) {
+          $this->_logger->addInfo("Pixlee did not return a status");
+        } elseif( is_object($response) && !$this->isBetween( $response->status, 200, 299 ) ) {
+          $errorMessage   = implode(',', (array)$response->message);
+          $this->_logger->addInfo("$response->status - $errorMessage ");
+        } else {
+          $this->_logger->addInfo("Analytics event sent");
+          return true;
+        }
+      }
+      $this->_logger->addInfo("Analytics event not sent ".json_encode($payload));
+      return false;
+    }
+
+    public function _extractProduct($product) {
+      $productData = array();
+      $productData['quantity'] = $product->getQty();
+
+      if($product->getId()) {
+        $productData['id']    = $product->getId();
+        $productData['name']  = $product->getName();
+        $productData['sku']   = $product->getSku();
+        $productData['price'] = $this->_pricingHelper->currency($product->getPrice(), true, false); // Get price in the main currency of the store. (USD, EUR, etc.)
+      }
+
+      return $productData;
+    }
+
+    public function _extractCart($quote) {
+      if(is_a($quote, '\Magento\Sales\Model\Quote') || is_a($quote, '\Magento\Sales\Model\Order')) {
+        $cartData = array('products' => array());
+        foreach ($quote->getAllItems() as $item) {
+          $cartData['products'][] = $this->_extractProduct($item);
+        }
+        $cartData['total'] = $quote->getGrandTotal();
+        return $cartData;
+      }
+      return false;
+    }
+
+    public function _extractCustomer($quote) {
+      if(is_a($quote, '\Magento\Sales\Model\Order')) {
+        return array(
+          'name'      => $quote->getShippingAddress()->getName(),
+          'email'     => $quote->getShippingAddress()->getEmail(),
+          'city'      => $quote->getShippingAddress()->getCity(),
+          'state'     => $this->_directoryRegion->load($quote->getShippingAddress()->getRegionId())->getName(),
+          'country'   => $quote->getShippingAddress()->getCountry()
+          );
+      }
+      return false;
+    }
+
+    public function _validateCredentials() {
+      try{
+        $this->_pixleeAPI->getAlbums();
+      }catch(Exception $e){
+        throw new \Magento\Framework\Exception\LocalizedException(__('You may have entered the wrong credentials. Please check again.'));
+      }
+    }
+
+    public function _preparePayload($extraData = array()) {
+      if(($payload = $this->_getPixleeCookie()) && $this->isActive()) {
+          // Append all extra data to the payload
+        foreach($extraData as $key => $value) {
+              // Don't accidentally overwrite existing data.
+          if(!isset($payload[$key])) {
+            $payload[$key] = $value;
+          }
+        }
+          // Required key/value pairs not in the payload by default.
+        $payload['API_KEY']= $this->getApiKey();
+        $payload['uid'] = $payload['CURRENT_PIXLEE_USER_ID'];
+        $payload['pixlee_album_photos_timestamps'] = $payload['CURRENT_PIXLEE_ALBUM_PHOTOS_TIMESTAMP'];
+        $payload['pixlee_album_photos'] = $payload['CURRENT_PIXLEE_ALBUM_PHOTOS'];
+
+        return http_build_query($payload);
+      }
+      $this->_logger->addInfo("Analytics event not sent because the cookie wasn't found");
+      return false; // No cookie exists,
+    }
+
+    protected function _getPixleeCookie() {
+      if(isset($_COOKIE['pixlee_analytics_cookie'])){
+        if($cookie = $_COOKIE['pixlee_analytics_cookie']) {
+              // Return the decoded cookie as an associative array, not a PHP object
+              // as json_decode prefers.
+          return json_decode($cookie, true);
+        }
+      }
+      return false;
+    }
+
+    protected function isBetween($theNum, $low, $high) {
+      if($theNum >= $low && $theNum <= $high) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
