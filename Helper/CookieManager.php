77d2de3692b446af24fc6d4c11b130bdabcc903d
Satnam Dhanoa <satnam@Pixlees-MacBook-Pro-4.local>
2016-02-19 16:34:00 -0800

    add class to read cookie

---
+++ Helper/CookieManager.php
@@ -0,0 +1,58 @@
+<?php 
+
+namespace Pixlee\Pixlee\Helper;
+
+use Magento\Framework\Session\SessionManagerInterface;
+use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
+use Magento\Framework\Stdlib\Cookie\PublicCookieMetadata;
+use Magento\Framework\Stdlib\CookieManagerInterface;
+
+class CookieManager
+{
+    /**
+     * Name of cookie that holds private content version
+     */
+    const COOKIE_NAME = 'pixlee_analytics_cookie';
+
+    /**
+     * CookieManager
+     *
+     * @var CookieManagerInterface
+     */
+    private $cookieManager;
+
+    /**
+     * @var CookieMetadataFactory
+     */
+    private $cookieMetadataFactory;
+
+    /**
+     * @var SessionManagerInterface
+     */
+    private $sessionManager;
+
+    /**
+     * @param CookieManagerInterface $cookieManager
+     * @param CookieMetadataFactory $cookieMetadataFactory
+     * @param SessionManagerInterface $sessionManager
+     */
+    public function __construct(
+        CookieManagerInterface $cookieManager,
+        CookieMetadataFactory $cookieMetadataFactory,
+        SessionManagerInterface $sessionManager
+    ) {
+        $this->cookieManager = $cookieManager;
+        $this->cookieMetadataFactory = $cookieMetadataFactory;
+        $this->sessionManager = $sessionManager;
+    }
+
+    /**
+     * Get form key cookie
+     *
+     * @return string
+     */
+    public function get()
+    {
+        return $this->cookieManager->getCookie(self::COOKIE_NAME);
+    }
+}
