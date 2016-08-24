98185b8d7e1aa4204c76cedeb6e6022f1d0dcf02
Jeff Chen <jeff@pixleeteam.com>
2016-08-23 17:04:23 -0700

    manage by instart

--- Helper/Data.php
+++ Helper/Data.php
@@ -13,7 +13,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     protected $_objectManager;
     protected $_logger;
 
-    const ANALYTICS_BASE_URL = 'https://limitless-beyond-4328.herokuapp.com/events/';
+    const ANALYTICS_BASE_URL = 'https://inbound-analytics.pixlee.com/events/';
     protected $_urls = array();
 
 	/**
