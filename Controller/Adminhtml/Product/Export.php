9e677536cb744fc3989d849b5c33213a9c36554b
Satnam Dhanoa <satnam@pixleeteam.com>
2017-08-28 14:37:10 -0700

    notify before and after

--- Controller/Adminhtml/Product/Export.php
+++ Controller/Adminhtml/Product/Export.php
@@ -31,6 +31,11 @@ class Export extends \Magento\Backend\App\Action
         if($this->_pixleeData->isActive()){
             $products = $this->_pixleeData->getUnexportedProducts();
             $products->getSelect();
+
+            $job_id = uniqid();
+            $counter = 0;
+            $this->notify_export_status('started', $job_id, count($products));
+
             foreach ($products as $product) {
                 $ids = $product->getStoreIds();
                 if(isset($ids[0])) {
@@ -39,6 +44,7 @@ class Export extends \Magento\Backend\App\Action
                 $response = $this->_pixleeData->exportProductToPixlee($product);
             }
 
+            $this->notify_export_status('finished', $job_id, $counter);
             $resultJson = $this->resultJsonFactory->create();
             return $resultJson->setData([
                 'message' => 'Success!',
@@ -49,5 +55,25 @@ class Export extends \Magento\Backend\App\Action
     private function _logPixleeMsg($message)
     {
         $this->_logger->addInfo("[Pixlee] :: ".$message);
+    }
+
+    protected function notify_export_status($status, $job_id, $num_products) {
+        $api_key = $this->_pixleeData->getApiKey();
+        $payload = array(
+            'api_key' => $api_key,
+            'status' => $status,
+            'job_id' => $job_id,
+            'num_products' => $num_products,
+            'platform' => 'magento_2'
+        );
+
+        $ch = curl_init('https://19978d4b.ngrok.io/api/v1/notifyExportStatus?api_key=' . $api_key);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json'
+        ));
+        $response = curl_exec($ch);
     }
 }
