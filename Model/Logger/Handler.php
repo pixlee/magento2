8eb57d1c70f36bccbfb20df894e31c80b4f51c59
Preston Rollins <preston.rollins@pixleeturnto.com>
2025-05-09 12:03:18 -0400

    TSD-358: Updates for Magento 2.4.8; update logger to only log info to custom file and log errors to exception log

--- Model/Logger/Handler.php
+++ Model/Logger/Handler.php
@@ -1,26 +1,24 @@
 <?php
 /**
- * Copyright © Pixlee TurnTo, Inc. All rights reserved.
+ * Copyright © Emplifi, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 declare(strict_types=1);
 
 namespace Pixlee\Pixlee\Model\Logger;
 
-use Magento\Framework\Logger\Handler\Base;
-use Monolog\Logger;
+use Magento\Framework\Logger\Handler\System;
+use Monolog\Level;
 
-class Handler extends Base
+class Handler extends System
 {
     /**
-     * Logging level
-     * @var int
-     */
-    protected $loggerType = Logger::INFO;
-
-    /**
-     * File name
      * @var string
      */
     protected $fileName = '/var/log/pixlee.log';
+
+    /**
+     * @var int
+     */
+    protected $loggerType = Level::Info;
 }
